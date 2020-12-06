0938afec3cae552b8844aef1489cff868da39417
ri2parna <ri2parna@outlook.com>
2020-12-06 19:10:11 +0530

    Commit Type: refactor()

--- App.js
+++ App.js
@@ -1,51 +1,36 @@
-import * as React from "react";
+import React from "react";
 import { Text, Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-
+// importing the required screens
+import SearchBars from "./components/components.SearchBar";
+import { HomeScreen } from "./pages/HomeScreen";
+import NotificationsScreen from "./pages/notificationScreen";
 import {
   PostScreen,
   ProfileScreen,
   SearchScreen,
   SettingsScreen,
 } from "./pages/Screens";
-
-import { StartScreen } from "./pages/StartScreen";
-import SearchBars from "./components/components.SearchBar";
-import { HomeScreen } from "./pages/HomeScreen";
-import DrawerScreen from "./pages/DrawerScreen";
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
+import StartScreen from "./pages/StartScreen";
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
+        <Drawer.Screen name="Home" component={StartScreen} />
+        <Drawer.Screen name="homePage" component={HomeScreen} />
         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
         <Drawer.Screen name="Profile" component={ProfileScreen} />
         <Drawer.Screen name="New Post" component={PostScreen} />
         <Drawer.Screen name="Settings" component={SettingsScreen} />
-        <Drawer.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            headerTitle: () => <SearchBars />,
-          }}
-        />
+        <Drawer.Screen name="Search" component={SearchScreen} options={ct} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const ct = {
+  headerTitle: () => <SearchBars />,
+};
--- pages/StartScreen.jsx
+++ pages/StartScreen.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { Button, Image, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import {
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from "react-native-gesture-handler";
 
 export default function StartScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Image
-        source={require("../assets/student-monochrome-400px.png")}
-        style={{ width: 200, height: 300, margin: 20 }}
+        style={styles.image_styles}
+        source={require("../assets/studentBackground.png")}
       />
-      <View style={{ position: "absolute", bottom: "10%" }}>
-        <Button
-          title="Get Started"
-          color="black"
-          onPress={() => {
-            navigation.navigate("homePage");
-          }}
-        />
-      </View>
+      <TouchableHighlight style={{ padding: "8%", backgroundColor: "cyan" }}>
+        <Text>Hotstuff</Text>
+      </TouchableHighlight>
+      <Text>Hello World</Text>
     </View>
   );
 }
-module.exports = { StartScreen };
+const styles = StyleSheet.create({
+  image_styles: {
+    width: "60%",
+    height: "60%",
+    margin: 20,
+  },
+});

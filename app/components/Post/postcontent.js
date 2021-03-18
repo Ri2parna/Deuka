b12e94dd1c20eaec24d3956cb4a6a1644738b59d
ri2parna <ri2parna@outlook.com>
2021-03-18 16:12:14 +0530

    Update postcontent.js

--- app/components/Post/postcontent.js
+++ app/components/Post/postcontent.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
 import BitterFontText from "../BitterFontText";
 
 export default function PostContent({ body }) {
-  return <BitterFontText size={16}>{children}</BitterFontText>;
+  return <BitterFontText size={16}>{body}</BitterFontText>;
 }
